d943b2f9855dad1b01c960863fc9bff81129cfa8
Ryan Parker <ryan.parker3@outlook.com>
2022-01-13 12:31:41 -0800

    update dependabot monthly

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-import { App, Stack } from 'aws-cdk-lib';
+import { App, Stack } from "aws-cdk-lib";
+import { Artifact } from "aws-cdk-lib/aws-codepipeline";
+import { LambdaInvokeAction } from "aws-cdk-lib/aws-codepipeline-actions";
+import { Function } from "aws-cdk-lib/aws-lambda";
 
 const app = new App();
-const stack = new Stack(app, 'sample');
+const stack = new Stack(app, "sample");
+
+declare const fn: Function;
+const sourceOutput = new Artifact();
+const buildOutput = new Artifact();
+const lambdaAction = new LambdaInvokeAction({
+  actionName: "Lambda",
+  inputs: [sourceOutput, buildOutput],
+  outputs: [new Artifact("Out1"), new Artifact("Out2")],
+  lambda: fn,
+});
